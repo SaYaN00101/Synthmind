1d6bd46bd662591fb5b7f5daf1669635e91c20c0
SaYaN00101 <158153243+SaYaN00101@users.noreply.github.com>
2025-06-23 01:24:32 +0530

    Update synthmind_data.sql

--- db/synthmind_data.sql
+++ db/synthmind_data.sql
@@ -1,1 +1,24 @@
+CREATE DATABASE IF NOT EXISTS synthmind_AI_DB;
+USE synthmind_AI_DB;
 
+CREATE TABLE user_data (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(250),
+    Age INT,
+    Gender VARCHAR(30),
+    Country VARCHAR(150),
+    City VARCHAR(250),
+    UserID VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    RegisteredAt DATETIME
+);
+
+CREATE TABLE chat_history (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID VARCHAR(100),
+    messege_role ENUM('user', 'assistant'),
+    message_content LONGTEXT,
+    DateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Session_ID VARCHAR(100),
+    Session_Title VARCHAR(250)
+);
